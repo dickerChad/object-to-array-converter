ef57ff4658c32b8d54074be046eacdb56e8c1226
dickerChad <souley4344@gmail.com>
2025-01-20 19:45:37 +0100

    add function

---
+++ convertObjet_Tableau.js
@@ -0,0 +1,11 @@
+// cette code permet de convertir un objet en un tableau
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+}
+const droneKeys = Object.keys(drone);
+droneKeys.forEach( function(key) {
+    result.push(key, drone[key])
+})
+console.log(result)
